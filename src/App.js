5b7d8351f05000a7f5b8d303648e246a1672a4da
MohamedMamdouh-98 <miidom96@gmail.com>
2023-12-23 14:40:26 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
+  const [products, setProducts] = useState([
+    { id: 1, title: "product1" },
+    { id: 2, title: "product2" },
+    { id: 3, title: "product3" },
+  ]);
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Navbar products= {products}/>
+      {products.map((item) => (
+        <h3 key={item.id}>
+          {item.id}. {item.title}
+        </h3>
+      ))}
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = ({ products }) => {
+  return (
+    <div>
+      Navbar
+      <h3>{products.length}</h3>
+    </div>
+  );
+};
+
+export default Navbar;
