e16827c3218dff1516551e2af0a42cad081ed659
MohamedMamdouh-98 <miidom96@gmail.com>
2023-12-23 14:42:40 +0200

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Navbar = ({ products }) => {
   return (
-    <div>
-      Navbar
-      <h3>{products.length}</h3>
+    <div >
+      
+      <h3>Navbar : {products.length}</h3>
     </div>
   );
 };
